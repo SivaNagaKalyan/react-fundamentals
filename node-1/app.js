5aacb1b5f4117b508110868340eb876c56538ef7
Siva Naga Kalyan <kalyan.kalyan.149@gmail.com>
2023-07-31 22:53:51 +0530

    Laying the foundation completion

--- node-1/app.js
+++ node-1/app.js
@@ -1,4 +1,7 @@
 // creating an element to render inside the root element.
+
+import React from "react";
+import  ReactDOM  from "react-dom/client";
 const title = React.createElement("div",{id:"container",},[
     React.createElement("h1",{className:"title"},"Hey React"),
     React.createElement("h4",{className:"small Heading"},"This is from Basic HTML")]
@@ -9,3 +12,4 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 //Rendering the root.
 root.render(title);
+
